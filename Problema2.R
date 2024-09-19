6734320f121c7ed41b38bb4a069837a5d6b8eae4
marc.lara.catasus <marc.lara.catasus@auleseebe.upc.edu>
2024-09-19 11:52:15 +0200

    p2 p3

--- Problema2.R
+++ Problema2.R
@@ -1,1 +1,6 @@
 #Codigo para problema 2
+
+rm(list=ls())
+mis_dades <- mtcars
+mis_dades$mpg
+hist(mis_dades$mpg)
--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,17 @@
 #Codigo para problema 3
+
+# crear vector
+x<- c(-8,5,2,-8,9,5,2,-3,1,-1,4,-4,9,3,-9,7,0,-7,8,-4,1,7,-6,4,5,-9,-2,-8,5,-5)
+x
+sum(x)
+
+z<- x[1:6]           #posició 1,2,3,4,5,6
+z
+y<- x[-c(21,2,26)]   #eliminar posicions 21,2,26  li resto un vector que té les pos que vull restar
+y
+
+sum(exp(x))-sum(exp(y))
+
+y[19]
+y[24]
+
